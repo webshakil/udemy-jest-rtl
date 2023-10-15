4120fd43d73426eaa78280cec549f5f8b4deeb55
webshakil <chumkysk@gmail.com>
2023-10-15 19:08:03 +0600

    only test is added

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,23 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/react app/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("render logo image",()=>{
+  render(<App/>)
+  const ImageElement = screen.getByRole("img")
+  expect(ImageElement).toBeInTheDocument();
+  expect(ImageElement).toHaveAttribute('src', 'logo.svg');
+  expect(ImageElement).toHaveAttribute('alt', 'logo');
+})
+
+test.only('renders Learn React Link',()=>{
+  render(<App/>)
+  const LinkElment = screen.getByRole('link')
+  expect(LinkElment).toBeInTheDocument();
+  expect(LinkElment).toHaveAttribute('href', 'https://reactjs.org');
+  expect(LinkElment).toHaveAttribute('target', "_blank");
+  expect(LinkElment).toHaveAttribute('rel', "noopener noreferrer");
+})
